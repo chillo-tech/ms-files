3b8aa1750642f9ee2a2f0a7e3c5b5bafd1f4d260
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-08-23 19:58:35 +0200

    Nom de la classe

--- src/main/java/tech/chillo/files/FilesHandler.java
+++ src/main/java/tech/chillo/files/FilesHandler.java
@@ -1,5 +1,6 @@
 package tech.chillo.files;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,10 +10,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.Map;
 
+@Slf4j
 @Component
 public class FilesHandler {
     final String basePath;
@@ -28,10 +31,13 @@ public class FilesHandler {
     )
     public void handleMessage(final Map<String, Object> params) throws IOException {
         final String filePath = String.valueOf(params.get("path"));
+        log.info("filePath {}", filePath);
         if (filePath != null && !filePath.equals("null") && Strings.isNotEmpty(filePath)) {
-            final String folder = filePath.substring(0, filePath.lastIndexOf("/"));
-            Files.createDirectories(Paths.get(folder));
             final String fullPath = String.format("%s/%s", this.basePath, filePath);
+            final Path folder = Paths.get(fullPath).getParent();
+            log.info("folder {}", folder);
+            Files.createDirectories(folder);
+            log.info("folder is created {}", Files.exists(folder));
 
             final String fileAsString = String.valueOf(params.get("file"));
             final byte[] decodedFile = Base64.getDecoder().decode(fileAsString);
