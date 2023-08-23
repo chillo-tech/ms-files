d4bdf3f0d95bb928ecc1665387c6983cd01c75ed
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-08-23 07:40:36 +0200

    Gestion des fichiers

---
+++ src/test/java/tech/chillo/files/MsFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.chillo.files;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
