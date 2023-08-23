ff172d0e64a2ebd1b1a8e7f0de785b1d033dee4c
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-08-23 18:59:34 +0200

    Nom de la classe

--- src/main/java/tech/chillo/files/FilesHandler.java
+++ src/main/java/tech/chillo/files/FilesHandler.java
@@ -1,11 +1,15 @@
 package tech.chillo.files;
 
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.Map;
 
@@ -18,20 +22,25 @@ public class FilesHandler {
     }
 
     @RabbitListener(
-
             queues = {"${spring.rabbitmq.template.queue}"},
             returnExceptions = "rabbitErrorHandler",
             errorHandler = "rabbitErrorHandler"
     )
     public void handleMessage(final Map<String, Object> params) throws IOException {
-        final String fileAsString = String.valueOf(params.get("file"));
-        final String path = String.valueOf(params.get("path"));
-        final String fullPath = String.format("%s/%s/%s", this.basePath, path, fileAsString);
-        final FileOutputStream outputStream = new FileOutputStream(fullPath);
-        final byte[] decodedFile = Base64.getDecoder().decode(fileAsString);
-        outputStream.write(decodedFile);
+        final String filePath = String.valueOf(params.get("path"));
+        if (filePath != null && !filePath.equals("null") && Strings.isNotEmpty(filePath)) {
 
-        outputStream.close();
+            final String folder = filePath.substring(0, filePath.lastIndexOf("/"));
+            Files.createDirectories(Paths.get(folder));
+            final String fullPath = String.format("%s/%s", this.basePath, filePath);
+
+            final String fileAsString = String.valueOf(params.get("file"));
+            final byte[] decodedFile = Base64.getDecoder().decode(fileAsString);
+,
+            try (final OutputStream stream = new FileOutputStream(fullPath)) {
+                stream.write(decodedFile);
+            }
+        }
+
     }
-
 }
