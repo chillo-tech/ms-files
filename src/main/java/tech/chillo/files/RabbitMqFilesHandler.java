59de7e2a9fec70a8da5558b9ca4030ab50dfb728
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-08-23 10:29:12 +0200

    paramètres

--- src/main/java/tech/chillo/files/RabbitMqFilesHandler.java
+++ src/main/java/tech/chillo/files/RabbitMqFilesHandler.java
@@ -10,10 +10,10 @@ import java.util.Base64;
 import java.util.Map;
 
 @Component
-public class RabbitMqFilesHandler {
+public class RabbitmqFilesHandler {
     final String basePath;
 
-    public RabbitMqFilesHandler(@Value("${application.files.base-path}") final String basePath) {
+    public RabbitmqFilesHandler(@Value("${application.files.base-path}") final String basePath) {
         this.basePath = basePath;
     }
 
@@ -25,7 +25,7 @@ public class RabbitMqFilesHandler {
     )
     public void handleMessage(final Map<String, Object> params) throws IOException {
         final String fileAsString = String.valueOf(params.get("file"));
-        final String path = String.valueOf(params.get("file"));
+        final String path = String.valueOf(params.get("path"));
         final String fullPath = String.format("%s/%s/%s", this.basePath, path, fileAsString);
         final FileOutputStream outputStream = new FileOutputStream(fullPath);
         final byte[] decodedFile = Base64.getDecoder().decode(fileAsString);
